c5f7ec254bbcfbe4ffcf6f8f09dc22102c2c904f
Jean-Luc KABORE-TURQUIN <turquinjl@gmail.com>
2023-09-04 19:29:53 +0200

    Set up Command & event

---
+++ src/main/java/com/linus/finance/challenge/command/LoanRepaymentCommand.java
@@ -0,0 +1,9 @@
+package com.linus.finance.challenge.command;
+
+public class LoanRepaymentCommand {
+    private Long loanId;
+    private double principalAmount;
+    private double interestAmount;
+
+    // Constructor, getters, and setters
+}
---
+++ src/main/java/com/linus/finance/challenge/event/LoanRepaymentEvent.java
@@ -0,0 +1,9 @@
+package com.linus.finance.challenge.event;
+
+public class LoanRepaymentEvent {
+    private Long loanId;
+    private double principalAmount;
+    private double interestAmount;
+
+    // Constructor, getters, and setters
+}
