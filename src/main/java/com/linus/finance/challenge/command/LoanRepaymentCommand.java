21626bb1da44e809def1258b14b8d7fcd34ed3f9
Jean-Luc KABORE-TURQUIN <turquinjl@gmail.com>
2023-09-04 21:29:38 +0200

    Add getters & setters

--- src/main/java/com/linus/finance/challenge/command/LoanRepaymentCommand.java
+++ src/main/java/com/linus/finance/challenge/command/LoanRepaymentCommand.java
@@ -5,5 +5,31 @@ public class LoanRepaymentCommand {
     private double principalAmount;
     private double interestAmount;
 
-    // Constructor, getters, and setters
+    // Constructor
+
+    // Getters
+    public Long getLoanId() {
+        return loanId;
+    }
+
+    public double getPrincipalAmount() {
+        return principalAmount;
+    }
+
+    public double getInterestAmount() {
+        return interestAmount;
+    }
+
+    // Setters
+    public Long setLoanId(Long loanId) {
+        return this.loanId = loanId;
+    }
+
+    public void setPrincipalAmount(double principalAmount) {
+        this.principalAmount = principalAmount;
+    }
+
+    public void setInterestAmount(double interestAmount) {
+        this.interestAmount = interestAmount;
+    }
 }
