e8b4f5c2deead4150f42e2ab0929f8ca95434202
Jean-Luc KABORE-TURQUIN <turquinjl@gmail.com>
2023-09-04 22:24:42 +0200

    Add constructor + getters

--- src/main/java/com/linus/finance/challenge/event/LoanRepaymentEvent.java
+++ src/main/java/com/linus/finance/challenge/event/LoanRepaymentEvent.java
@@ -1,9 +1,27 @@
 package com.linus.finance.challenge.event;
 
 public class LoanRepaymentEvent {
-    private Long loanId;
-    private double principalAmount;
-    private double interestAmount;
+    private final String loanId;
+    private final double principalAmount;
+    private final double interestAmount;
 
-    // Constructor, getters, and setters
+    // Constructor
+    public LoanRepaymentEvent(String loanId, double principalAmount, double interestAmount) {
+        this.loanId = loanId;
+        this.principalAmount =principalAmount;
+        this.interestAmount = interestAmount;
+    }
+
+    public String getLoanId() {
+        return loanId;
+    }
+
+    public double getPrincipalAmount() {
+        return principalAmount;
+    }
+
+    public double getInterestAmount() {
+        return interestAmount;
+    }
+
 }
