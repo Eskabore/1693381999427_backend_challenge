045bb562537e302557c15cacd66d73c024a6976d
Jean-Luc KABORE-TURQUIN <turquinjl@gmail.com>
2023-09-04 23:29:37 +0200

    modified command handler
    Replaced Repository<Loan> loanRepository with CommandGateway to easily publish command messages

--- src/main/java/com/linus/finance/challenge/handlers/LoanCommandHandler.java
+++ src/main/java/com/linus/finance/challenge/handlers/LoanCommandHandler.java
@@ -1,8 +1,9 @@
 package com.linus.finance.challenge.handlers;
 
+import com.linus.finance.challenge.aggregate.LoanAggregate;
 import com.linus.finance.challenge.command.LoanRepaymentCommand;
-import com.linus.finance.challenge.model.Loan;
 import org.axonframework.commandhandling.CommandHandler;
+import org.axonframework.commandhandling.gateway.CommandGateway;
 import org.axonframework.modelling.command.Repository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -10,11 +11,10 @@ import org.springframework.stereotype.Service;
 @Service
 public class LoanCommandHandler {
     @Autowired
-    private Repository<Loan> loanRepository;
+    private CommandGateway commandGateway;
 
     @CommandHandler
     public void handle(LoanRepaymentCommand command) {
-        Loan loan = loanRepository.load(command.getLoanId());
-        loan.repay(command.getPrincipalAmount(), command.getInterestAmount());
+       commandGateway.sendAndWait(command);
     }
 }
