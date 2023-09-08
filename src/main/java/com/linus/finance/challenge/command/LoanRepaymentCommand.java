eee798f2d6fccc8071edfd9488fc9a93e532f22e
Jean-Luc KABORE-TURQUIN <turquinjl@gmail.com>
2023-09-08 08:28:48 +0200

    Update LoanRepaymentCommand

--- src/main/java/com/linus/finance/challenge/command/LoanRepaymentCommand.java
+++ src/main/java/com/linus/finance/challenge/command/LoanRepaymentCommand.java
@@ -2,14 +2,22 @@ package com.linus.finance.challenge.command;
 
 import java.util.UUID;
 
-// LoanRepaymentCommand.java
 public class LoanRepaymentCommand {
 
     private final UUID loanId;
     private final double repaymentAmount;
-    // other fields
+
+    public LoanRepaymentCommand(UUID loanId, double repaymentAmount) {
+        this.loanId = loanId;
+        this.repaymentAmount = repaymentAmount;
+    }
 
     // Getters and setters
-    // ...
-}
+    public UUID getLoanId() {
+        return loanId;
+    }
 
+    public double getRepaymentAmount() {
+        return repaymentAmount;
+    }
+}
