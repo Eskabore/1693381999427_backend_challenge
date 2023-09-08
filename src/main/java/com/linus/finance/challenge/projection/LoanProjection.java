567d59bb5e5445e2e001201ecb235abb6d311bb5
Jean-Luc KABORE-TURQUIN <turquinjl@gmail.com>
2023-09-08 07:39:33 +0200

    Implement controllers

---
+++ src/main/java/com/linus/finance/challenge/projection/LoanProjection.java
@@ -0,0 +1,25 @@
+package com.linus.finance.challenge.projection;
+
+import org.axonframework.eventhandling.EventHandler;
+
+import com.linus.finance.challenge.event.LoanCreatedEvent;
+import com.linus.finance.challenge.event.LoanRepaymentEvent;
+
+public class LoanProjection {
+
+    @EventHandler
+    public void on(LoanCreatedEvent event, LoanViewRepository repository) {
+        LoanView loanView = new LoanView();
+        loanView.setLoanId(event.getLoanId());
+        loanView.setPrincipal(event.getPrincipal());
+        loanView.setInterest(event.getInterest());
+        // set other fields
+
+        repository.save(loanView);
+    }
+
+    @EventHandler
+    public void on(LoanRepaymentEvent event, LoanViewRepository repository) {
+        // Update the LoanView based on the repayment event
+    }
+}
---
+++ src/main/java/com/linus/finance/challenge/projection/LoanView.java
@@ -0,0 +1,15 @@
+package com.linus.finance.challenge.projection;
+
+import java.util.UUID;
+
+public class LoanView {
+
+    private UUID loanId;
+    private double principal;
+    private double interest;
+    // other fields like remaining balance, status, etc.
+
+    // Getters and setters
+    // ...
+}
+
