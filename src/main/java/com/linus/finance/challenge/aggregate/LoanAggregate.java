0a16fc164dfc8911a5a02f1c0c89f5738bdadc8d
Jean-Luc KABORE-TURQUIN <turquinjl@gmail.com>
2023-09-08 07:11:51 +0200

    Implement event handlers within LoanAggregate

--- src/main/java/com/linus/finance/challenge/aggregate/LoanAggregate.java
+++ src/main/java/com/linus/finance/challenge/aggregate/LoanAggregate.java
@@ -3,12 +3,15 @@ package com.linus.finance.challenge.aggregate;
 import java.util.UUID;
 
 import org.axonframework.commandhandling.CommandHandler;
+import org.axonframework.eventsourcing.EventSourcingHandler;
 import org.axonframework.modelling.command.AggregateIdentifier;
 import org.axonframework.modelling.command.AggregateLifecycle;
 import org.axonframework.spring.stereotype.Aggregate;
 
 import com.linus.finance.challenge.command.CreateLoanCommand;
 import com.linus.finance.challenge.command.LoanRepaymentCommand;
+import com.linus.finance.challenge.event.LoanCreatedEvent;
+import com.linus.finance.challenge.event.LoanRepaymentEvent;
 
 @Aggregate
 public class LoanAggregate {
@@ -26,10 +29,23 @@ public class LoanAggregate {
     // Command-handling constructors and methods
     @CommandHandler
     public LoanAggregate(CreateLoanCommand command) {
-        AggregateLifecycle.apply(new LoanCreatedEvent(command.getLoanId(), command.getPrincipal(), command.getInterest()));
+        AggregateLifecycle
+                .apply(new LoanCreatedEvent(command.getLoanId(), command.getPrincipal(), command.getInterest()));
     }
 
     // Event-sourcing handlers
-    // ...
+    @EventSourcingHandler
+    public void on(LoanCreatedEvent event) {
+        this.loanId = event.getLoanId();
+        this.principal = event.getPrincipal();
+        this.interest = event.getInterest();
+        // Update other fields
+    }
+
+    // Command-handling methods
+    @EventSourcingHandler
+    public void on(LoanRepaymentEvent event) {
+        // Update other fields
+    }
 
 }
