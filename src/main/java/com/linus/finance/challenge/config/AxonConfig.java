745fc613a23fe933a409ec30bcdbc65f70fa6193
Jean-Luc KABORE-TURQUIN <turquinjl@gmail.com>
2023-09-08 09:13:25 +0200

    Create and set up AxonConfig

---
+++ src/main/java/com/linus/finance/challenge/config/AxonConfig.java
@@ -0,0 +1,39 @@
+package com.linus.finance.challenge.config;
+
+import org.axonframework.commandhandling.CommandBus;
+import org.axonframework.commandhandling.SimpleCommandBus;
+import org.axonframework.eventhandling.EventBus;
+import org.axonframework.eventhandling.SimpleEventBus;
+import org.axonframework.eventsourcing.eventstore.EventStorageEngine;
+import org.axonframework.eventsourcing.eventstore.inmemory.InMemoryEventStorageEngine;
+import org.axonframework.messaging.annotation.ParameterResolverFactory;
+
+import org.axonframework.spring.config.annotation.SpringBeanParameterResolverFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AxonConfig {
+
+    @Bean
+    public CommandBus commandBus() {
+        return SimpleCommandBus.builder().build();
+    }
+
+    @Bean
+    public EventBus eventBus() {
+        return SimpleEventBus.builder().build();
+    }
+
+    @Bean
+    public EventStorageEngine eventStorageEngine() {
+        return new InMemoryEventStorageEngine();
+    }
+
+    @Bean
+    public ParameterResolverFactory parameterResolverFactory() {
+        return new SpringBeanParameterResolverFactory();
+    }
+
+}
+
