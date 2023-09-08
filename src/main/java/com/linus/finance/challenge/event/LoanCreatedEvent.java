c8792451645c1497ea8efc696d91391339d5889c
Jean-Luc KABORE-TURQUIN <turquinjl@gmail.com>
2023-09-08 07:02:44 +0200

    Update events

---
+++ src/main/java/com/linus/finance/challenge/event/LoanCreatedEvent.java
@@ -0,0 +1,14 @@
+package com.linus.finance.challenge.event;
+
+import java.util.UUID;
+
+public class LoanCreatedEvent {
+
+    private final UUID loanId;
+    private final double principal;
+    private final double interest;
+    // other fields
+
+    // Getters and setters
+    // ...
+}
---
+++ src/main/java/com/linus/finance/challenge/event/LoanRepaymentEvent.java
@@ -0,0 +1,13 @@
+package com.linus.finance.challenge.event;
+
+import java.util.UUID;
+
+public class LoanRepaymentEvent {
+
+    private final UUID loanId;
+    private final double repaymentAmount;
+    // other fields
+
+    // Getters and setters
+    // ...
+}
